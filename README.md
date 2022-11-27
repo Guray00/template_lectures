ec162d00185b3f196e5220e346f98d0e73a6b7bf
Marco <30523962+Guray00@users.noreply.github.com>
2022-11-27 12:01:59 +0100

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Lectures Template
+
+Template per gli appunti delle lezioni.
+
+
+## Installazione
+
+Guida per l'installazione del template.
+
+### Download template
+
+Scarica questa repository di esempio:
+
+```bash
+git clone https://github.com/Guray00/template_lectures
+```
+
+oppure attraverso il comando scaricando da questa pagina come zip da [qui](https://github.com/Guray00/template_lectures/archive/refs/heads/master.zip).
+
+### Estrai la cartella
+Estrai la cartella, ed entra al suo interno.
+
+### Installa le dipendenze
+
+Installa le dipendenze necessarie per la compilazione dei file eseguendo il file `install.bat`.
+
+### Rimuovi file inutili
+
+Cancella `install.bat`.
+
+### Inizia!
+
+Inizia la scrittura dei tuoi appunti!
---
+++ install.bat
@@ -0,0 +1,69 @@
+@ECHO OFF
+
+:: file temporaneo
+set TMPFOLDER=C:\Users\%username%\AppData\Local\Temp
+
+:: scritta iniziale
+echo Benvenuto nell'installer del template
+echo verra' installato, in ordine:
+echo - latex
+echo - pandoc
+echo - template per PDF
+echo - template per WEB
+echo: 
+
+:: INSTALLAZIONE LATEX
+echo Verifico se latex e' installato...
+latex --version > nul || (
+echo Latex non installato, download in corso...
+
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://miktex.org/download/ctan/systems/win32/miktex/setup/windows-x64/basic-miktex-22.10-x64.exe', 'latex.exe')"
+
+echo latex scaricato
+echo installazione di latex in corso, prosegui nella finestra che si aprirà a breve!
+.\latex.exe
+
+latex --version > nul || (
+echo latex non e' stato installato, abortisco l'installazione
+exit
+)
+
+DEL latex.exe
+)
+echo latex installato.
+echo: 
+
+:: INSTALLAZIONE PANDOC
+echo Verifico se pandoc e' installato...
+pandoc --version > nul || (
+echo pandoc non installato, download in corso...
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-windows-x86_64.msi', 'pandoc.msi')"
+echo pandoc scaricato
+echo installazione di pandoc in corso, prosegui nella finestra che si aprirà a breve!
+.\pandoc.msi
+
+pandoc --version > nul || (
+echo pandoc non e' stato installato, abortisco l'installazione
+exit
+)
+
+DEL ./pandoc.msi
+)
+echo pandoc installato.
+echo: 
+
+echo Sto copiando i template...
+:: creo il path per i template
+if not exist "C:\Users\%username%\AppData\Roaming\pandoc\templates" mkdir C:\Users\%username%\AppData\Roaming\pandoc\templates
+
+:: scarico template latex
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Wandmalfarbe/pandoc-latex-template/master/eisvogel.tex', 'C:\Users\%username%\AppData\Roaming\pandoc\templates\eisvogel.tex')"
+
+
+:: scarico template web
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ryangrose/easy-pandoc-templates/master/html/elegant_bootstrap_menu.html', 'C:\Users\%username%\AppData\Roaming\pandoc\templates\elegant_bootstrap_menu.html')"
+echo template copiati.
+echo:
+
+echo Installazione terminata.
+pause
